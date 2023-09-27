a9730740a066189e53185bebada12a69043f6f21
Miguel Ramos <return.infox@gmail.com>
2023-09-26 21:31:13 -0300

    codigo asincrono

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+function sleep(ms:any) {
+  const wakeUpTime = Date.now() + ms;
+  while(Date.now() < wakeUpTime) {}
+}
+
 //* localhost:3000/
 app.get('/', (require:any, response:any) => {
   //* MALA PRÁCTICA (lectura de sincrónica de archivos)
@@ -17,10 +22,17 @@ app.get('/', (require:any, response:any) => {
 });
 
 //* localhost:3000/normal
-
 app.get('/normal', (req:any, res:any) => {
   res.send('Ejecución normal....');
 });
+
+
+app.get('/block', (req:any, res: any)  => {
+  // * MALA PRACTICA 
+  sleep(10000); // * Duerme por 10 segundos
+  res.send('Blocking operation completed');
+});
+
 
 
 app.listen(port, () => {
